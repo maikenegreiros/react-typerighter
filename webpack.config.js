93b0b7312c3866ef0817033f3f3355325decef14
Maike <maike_jbe@hotmail.com>
2020-04-26 09:35:52 -0300

    chore(build): Optmized library build

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 const path = require("path");
+const pkg = require("./package.json");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "./"),
     filename: "./index.js",
+    library: pkg.name,
+    libraryTarget: "commonjs2",
   },
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
         test: /\.js$/,
+        exclude: [/(node_modules)/],
         use: {
           loader: "babel-loader",
-          options: {
-            exclude: [/(node_modules)/],
-          },
         },
       },
     ],
